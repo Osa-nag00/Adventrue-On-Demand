1e5df51af7e2889179dc90f794bbd250e6edcfe9
Osa-nag00 <naghiseosa2000@gmail.com>
2023-11-01 21:37:05 -0400

    Hello World Page

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
 	return (
 		<>
-			<div className='text-9xl flex flex-row'>Hello World!</div>
+			<div className='text-9xl flex flex-row justify-center'>Hello World!</div>
 		</>
 	);
 }
