e3b4133a0e35c21e6f8f634f9669750cede23ebd
Gustavo Nazario Perez <gustavonazario27@gmail.com>
2023-11-24 11:05:44 -0500

    Chatbot API v1, working with Next.js app router

---
+++ src/login/auth.js
@@ -0,0 +1,46 @@
+import dotenv from 'dotenv';
+import passport from 'passport';
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
+import { User } from './models/userModel.js';
+dotenv.config({ path: '../../.env' });
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:5555/google/callback",
+    passReqToCallback: true
+  },
+  function(request, accessToken, refreshToken, profile, done) {
+    // Check if user exists in the db
+    User.findOne({ email : profile._json.email }).then((currentUser) => {
+      if (currentUser) {
+        console.log("current user is: ", currentUser);
+        done(null, currentUser);
+      }
+
+      // Else create new user in db
+      else {
+        new User({
+          email: profile._json.email,
+        })
+        .save()
+        .then((newUser) => {
+          console.log("new user created: " + newUser);
+          done(null, newUser);
+        })
+      }
+    });
+  }
+));
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+    User.findById(id).then((user) => {
+      done(null, user);
+    });
+});
+
+
--- src/pages/api/example.js
+++
@@ -1,4 +0,0 @@
-export default (req, res) => {
-    const data = { message: 'Hello from the HERE!' };
-    res.json(data);
-  };
