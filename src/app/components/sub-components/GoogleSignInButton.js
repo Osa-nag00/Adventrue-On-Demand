37403f29b7a3a385c5120bebe1d1622cd6d5d86a
Osa-nag00 <naghiseosa2000@gmail.com>
2023-11-26 14:37:12 -0500

    Remove unused files and update dependencies

--- src/app/components/sub-components/GoogleSignInButton.js
+++ src/app/components/sub-components/GoogleSignInButton.js
@@ -9,10 +9,10 @@ export default function GoogleSignInButton() {
 				signIn("google");
 			}}
 			type='button'
-			class='text-white bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 mr-2 mb-2'
+			className='text-white bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 mr-2 mb-2'
 		>
 			<svg
-				class='mr-2 -ml-1 w-4 h-4'
+				className='mr-2 -ml-1 w-4 h-4'
 				aria-hidden='true'
 				focusable='false'
 				data-prefix='fab'
